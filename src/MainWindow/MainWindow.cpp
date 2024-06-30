7257f42a3de16724a02e311b159357904fbcaf17
ElxNacho <74469929+ElxNacho@users.noreply.github.com>
2024-06-30 03:10:55 +0200

    a

--- src/MainWindow/MainWindow.cpp
+++ src/MainWindow/MainWindow.cpp
@@ -1,23 +1,176 @@
 #include "MainWindow.h"
 #include "../global/Global.h"
 
-
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
     global::geometry(this);
 
-    /////////////
-    // Sidebar //
-    /////////////
+    decryptPasswords();
 
+    setupSidebar();
+    setupTopbar();
+    setupCenterPasswordList();
+}
+
+void MainWindow::changeSort() {
+    // Implement sorting logic here
+}
+
+void MainWindow::newEntry() {
+    // Implement new entry logic here
+}
+
+void MainWindow::showPasswords() {
+    // Implement show passwords logic here
+}
+
+void MainWindow::showPasswordGen() {
+    // Implement password generation logic here
+}
+
+void MainWindow::delPassword() {
+    // Implement delete password logic hier
+}
+
+void MainWindow::editPassword() {
+    // Implement edit password logic hier
+}
+
+void MainWindow::ChangeMasterPassword() {
+    // Implement change master password logic hier
+}
+
+void MainWindow::lockScreen() {
+    global::openLockscreen(this);
+}
+
+QString MainWindow::genKey() {
+    QString key;
+    QVector<int> posNums;
+
+    for (int i = 0; i < allChars.size(); ++i) {
+        int random_num;
+        bool num_in_list;
+        do {
+            random_num = QRandomGenerator::global()->bounded(allChars.size());
+            num_in_list = false;
+            for (int j = 0; j < i; j++) {
+                if (posNums[j] == random_num) {
+                    num_in_list = true;
+                    break;
+                }
+            }
+        } while (num_in_list);
+        posNums.push_back(random_num);
+        key += allChars[random_num];
+    }
+
+    qDebug() << "The Gen Key is: " << key;
+    return key;
+}
+
+QString MainWindow::genPassword(int length, QString allowedChars) {
+    QString password;
+    for (int i = 0; i < length; ++i) {
+        int randomInt = QRandomGenerator::global()->bounded(allowedChars.size());
+        password += allowedChars[randomInt];
+    }
+
+    qDebug() << "The Gen Password is: " << password;
+    return password;
+}
+
+void MainWindow::encryptPasswords() {
+    QString filePath = "../res/passwords.txt";
+    QFile file(filePath);
+
+    if (!file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+        qDebug() << "Fehler beim Öffnen der Datei zum Schreiben:" << file.errorString();
+        return;
+    }
+
+    QTextStream out(&file);
+
+    QString key = genKey();
+    out << key + "\n";
+    for (const QVector<QString>& vec : _passwordList) {
+        for (const QString& str : vec) {
+            out << encryptString(str, key) << "\n";
+        }
+    }
+
+    file.close();
+
+    qDebug() << "Strings wurden erfolgreich in" << filePath << "geschrieben.";
+}
+
+void MainWindow::decryptPasswords() {
+    QString filePath = "../res/passwords.txt";
+    QFile file(filePath);
+
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        qDebug() << "Fehler beim Öffnen der Datei zum Lesen:" << file.errorString();
+        return;
+    }
+
+    QTextStream in(&file);
+    _passwordList.clear();
+    bool keyPos = true;
+    QString key;
+    while (!in.atEnd()) {
+        QString line = in.readLine();
+        QVector<QString> tempVec;
+        if (keyPos) {
+            key = line;
+            keyPos = false;
+            continue;
+        }
+        tempVec.push_back(decryptString(line, key));
+        _passwordList.push_back(tempVec);
+    }
+
+    file.close();
+    qDebug() << "Strings wurden erfolgreich aus" << filePath << "gelesen.";
+}
+
+QString MainWindow::encryptString(const QString &input, const QString &key) {
+    QString output;
+    output.resize(input.size());
+
+    for (int i = 0; i < input.size(); ++i) {
+        QChar currentChar = input.at(i);
+        int index = allChars.indexOf(currentChar);
+        if (index != -1) {
+            output[i] = key.at(index);
+        } else {
+            output[i] = currentChar;
+        }
+    }
+    return output;
+}
+
+QString MainWindow::decryptString(const QString &input, const QString &key) {
+    QString output;
+    output.resize(input.size());
+
+    for (int i = 0; i < input.size(); ++i) {
+        QChar currentChar = input.at(i);
+        int index = key.indexOf(currentChar);
+        if (index != -1) {
+            output[i] = allChars.at(index);
+        } else {
+            output[i] = currentChar;
+        }
+    }
+    return output;
+}
+
+void MainWindow::setupSidebar() {
     // Geometric
     int logoHeight = 100;
     int logoWidth = 210;
-
     int spacingButtonSidebar = 10;
     int spacingSide = 10;
-
     int buttonsInSideBar = 7;
-
     int buttonWidthSidebar = logoWidth - spacingSide;
     int buttonHeightSidebar = (global::screenHeight - logoHeight - spacingButtonSidebar * (buttonsInSideBar + 1)) / buttonsInSideBar;
 
@@ -29,7 +182,7 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
     _LabelLogo->setAlignment(Qt::AlignCenter);
     _LabelLogo->setFixedSize(logoWidth, logoHeight);
 
-    // sidebar area
+    // Sidebar area
     _sidebarWidget = new QWidget(this);
     _sidebarWidget->setFixedWidth(buttonWidthSidebar);
 
@@ -37,14 +190,11 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
     _sidebarLayout->setSpacing(spacingButtonSidebar);
     _sidebarLayout->setContentsMargins(spacingSide, spacingSide, spacingSide, spacingSide);
 
-    _sidebarWidget->setLayout(_sidebarLayout);
-
-    // Buttons
     _ButtonAddEntry = new QPushButton("Add Entry", _sidebarWidget);
     _ButtonAddEntry->setFixedSize(buttonWidthSidebar, buttonHeightSidebar);
     _sidebarLayout->addWidget(_ButtonAddEntry);
 
-    _ButtonShowPasswords = new QPushButton("Show Password", _sidebarWidget);
+    _ButtonShowPasswords = new QPushButton("Show Passwords", _sidebarWidget);
     _ButtonShowPasswords->setFixedSize(buttonWidthSidebar, buttonHeightSidebar);
     _sidebarLayout->addWidget(_ButtonShowPasswords);
 
@@ -64,17 +214,18 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
     _ButtonChangeMasterPassword->setFixedSize(buttonWidthSidebar, buttonHeightSidebar);
     _sidebarLayout->addWidget(_ButtonChangeMasterPassword);
 
-    _ButtonCloseMenu = new QPushButton("Close Menu", _sidebarWidget);
-    _ButtonCloseMenu->setFixedSize(buttonWidthSidebar, buttonHeightSidebar);
-    _sidebarLayout->addWidget(_ButtonCloseMenu);
+    _ButtonOpenLockscreen = new QPushButton("Close Menu", _sidebarWidget);
+    _ButtonOpenLockscreen->setFixedSize(buttonWidthSidebar, buttonHeightSidebar);
+    _sidebarLayout->addWidget(_ButtonOpenLockscreen);
 
+    _sidebarWidget->setLayout(_sidebarLayout);
     _sidebarWidget->setGeometry(spacingSide, logoHeight, logoWidth, global::screenHeight - logoHeight);
     _sidebarWidget->show();
 
-    ////////////
-    // Topbar //
-    ////////////
+    connect(_ButtonOpenLockscreen, &QPushButton::clicked, this, &MainWindow::lockScreen);
+}
 
+void MainWindow::setupTopbar() {
     int topbarHeight = 40;
 
     _topBarWidget = new QWidget(this);
@@ -93,43 +244,54 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
     _topBarLayout->addWidget(_inputSearch);
     _topBarLayout->addWidget(_ButtonStartSearch);
 
-    _topBarWidget->setGeometry(logoWidth, 0, global::screenWidth - logoWidth, logoHeight);
+    _topBarWidget->setGeometry(210, 0, global::screenWidth - 210, 100);
     _topBarWidget->show();
-
-    ////////////
-    // center //
-    ////////////
-
 }
 
-void MainWindow::changeSort() {
+void MainWindow::setupCenterPasswordList() {
+    _centerPasswordList = new QWidget(this);
+    _scrollArea = new QScrollArea(this);
+    _gridLayout = new QGridLayout();
 
+    populateGrid();
+
+    QWidget *scrollWidget = new QWidget();
+    scrollWidget->setLayout(_gridLayout);
+    _scrollArea->setWidget(scrollWidget);
+    _scrollArea->setWidgetResizable(true);
+    _scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+    _scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+
+    QVBoxLayout *layout = new QVBoxLayout(_centerPasswordList);
+    layout->addWidget(_scrollArea);
+
+    _centerPasswordList->setLayout(layout);
+    _centerPasswordList->setGeometry(210, 100, global::screenWidth - 210, global::screenHeight - 100);
+    _centerPasswordList->show();
 }
 
-void MainWindow::newEntry() {
+void MainWindow::populateGrid() {
+    QFont font("Arial", 12);
+    int row = 0;
 
-}
+    QLabel *headerWebsite = new QLabel("Website");
+    QLabel *headerUsername = new QLabel("Username/Email");
+    QLabel *headerPassword = new QLabel("Password");
 
-void MainWindow::showPasswords() {
+    headerWebsite->setFont(font);
+    headerUsername->setFont(font);
+    headerPassword->setFont(font);
 
-}
+    _gridLayout->addWidget(headerWebsite, row, 0);
+    _gridLayout->addWidget(headerUsername, row, 1);
+    _gridLayout->addWidget(headerPassword, row, 2);
 
-void MainWindow::showPasswordGen() {
-
-}
-
-void MainWindow::delPassword() {
-
-}
-
-void MainWindow::editPassword() {
-
-}
-
-void MainWindow::ChangeMasterPassword() {
-
-}
-
-void MainWindow::lockScreen() {
-
+    for (int i = 0; i < _passwordList.size(); ++i) {
+        row++;
+        for (int j = 0; j < _passwordList[i].size(); ++j) {
+            QLabel *label = new QLabel(_passwordList[i][j]);
+            label->setFont(font);
+            _gridLayout->addWidget(label, row, j);
+        }
+    }
 }
--- src/MainWindow/MainWindow.h
+++ src/MainWindow/MainWindow.h
@@ -1,7 +1,3 @@
-//
-// Created by prang on 27.04.2024.
-//
-
 #ifndef PASSWORDMANAGERCPPQT_MAINWINDOW_H
 #define PASSWORDMANAGERCPPQT_MAINWINDOW_H
 
@@ -13,6 +9,18 @@
 #include <QTextEdit>
 #include <QVBoxLayout>
 #include <QHBoxLayout>
+#include <QVector>
+#include <QString>
+#include <QListWidget>
+#include <QScrollArea>
+#include <QDebug>
+#include <QRandomGenerator>
+#include <QFont>
+#include <QMessageBox>
+#include <QMouseEvent>
+#include <QClipboard>
+#include <QFile>
+#include <QGridLayout>
 
 class MainWindow : public QMainWindow {
 Q_OBJECT
@@ -38,7 +46,7 @@ private:
     QPushButton *_ButtonShowPasswords;
     QPushButton *_ButtonShowPasswordGen;
     QPushButton *_ButtonAddEntry;
-    QPushButton *_ButtonCloseMenu;
+    QPushButton *_ButtonOpenLockscreen;
     QPushButton *_ButtonChangeMasterPassword;
     QPushButton *_ButtonDelPassword;
     QPushButton *_ButtonEditPassword;
@@ -50,5 +58,33 @@ private:
     QPushButton *_ButtonChangeSort;
     QLineEdit *_inputSearch;
 
+    // Center Widget
+    QWidget *_centerPasswordList;
+    QScrollArea *_scrollArea;
+    QGridLayout *_gridLayout;
+
+    // Data
+    QVector<QVector<QString>> _passwordList;
+
+    // Helper functions
+    void setupSidebar();
+    void setupTopbar();
+    void setupCenterPasswordList();
+    void populateGrid();
+
+    void readPasswordFromFile();
+    void writePasswordsInFile();
+
+    void copySelectedText(QListWidgetItem *item);
+
+    const QString allChars = " ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+    QString genKey();
+    QString genPassword(int length, QString allowedChars);
+    QString decryptString(const QString &input, const QString &key);
+    QString encryptString(const QString &input, const QString &key);
+
+    void encryptPasswords();
+    void decryptPasswords();
 };
-#endif //PASSWORDMANAGERCPPQT_MAINWINDOW_H
+
+#endif // PASSWORDMANAGERCPPQT_MAINWINDOW_H
